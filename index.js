ace164d4461590434c16c62e80109b7248b70e11
Saibot2001 <tobiascomas81@gmail.com>
2024-06-04 12:58:31 -0300

    4-6 13pm

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 //Declaracion
 let aux = 0;
 let menu = 0;
-const inventarioTienda = ["Sillon" , "Silla" , "Mesa"] ;
-let carrito = [] ;
+const inventarioTienda = [{id : 1 , producto: "Silla" , precio: 1000} ,
+                          {id : 2 , producto: "Sillon", precio: 1500} ,
+                          {id : 3 , producto: "Mesa", precio: 2000} ,
+                          {id : 4 , producto: "Puerta", precio: 3500} ,
+                          {id : 5 , producto: "Banco", precio: 500}] ;
+const carrito = [] ;
 
 function menuCarrito(){
 
@@ -32,4 +36,31 @@ function menuCarrito(){
                             }
 }
 
-menuCarrito();
+
+function agregar(){
+    //Cargar
+    console.log("hola")
+    aux = document.getElementById("carrito").value;
+    carrito.push(inventarioTienda[aux-1])
+     
+}
+
+
+//Vaciar
+function vaciar(carrito){
+    carrito = [];
+}
+
+//Imprimir carrito
+function print(){
+    let total = 0;
+    for (const articulo of carrito){
+        let contenedorCarrito = document.createElement("div");
+        contenedorCarrito.innerHTML= ` <h3>${articulo.producto}  $ ${articulo.precio} </h3> `
+        document.body.appendChild(contenedorCarrito);
+        total = total + articulo.precio;
+    }
+    let contenedorTotal = document.createElement("divTotal");
+    contenedorTotal.innerHTML= ` <h3>Total : $ ${total}</h3> `;
+    document.body.appendChild(contenedorTotal);
+}
